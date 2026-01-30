7e9ad86b2b62697e4baadb1af9e052af91335b19
Naqeeb Rahmani <naqeeb.rahmani@learnet.se>
2026-01-30 12:02:40 +0100

    Got a sprite on the screen

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-import pygame
+import pygame, time
 
+#Day 1: Get atleast 1 car, which should be able to move 
 
+#SCREEN
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+screen = (pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)))
+##################################
 
 #STATE
 game = "running"
 #############################
 
-pygame.init
+#CARS
+car_1 = pygame.image.load(r"C:\Users\naqeeb.rahmani\Desktop\ProfilVecka(2026)\Racing_game\assets\cars\car_player1.png")
+########################
 
-screen = (pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)))
+pygame.init()
+
 
 pygame.display.set_caption("Racing-Game")
 
 
+screen.blit(car_1, (100,100))
 
-
+pygame.display.update()
 
 while game == "running":
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = "not running"
